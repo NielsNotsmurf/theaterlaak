9a16b2103855b29c509e92979c5e8bd6ed2ae521
Joeri <joeri@famdehoog.nl>
2022-12-16 17:04:08 +0100

    added mui to application

---
+++ REACT/frontend-laak/src/Theme/Styles/palette.js
@@ -0,0 +1,20 @@
+
+const palette = {
+    background: {
+        default: "#000814"
+    },
+    text: {
+        primary: "#FFFFFF",
+        secondary: "#111111"
+    },
+    primary: {
+        light: "#003566",
+        main: "#001D3D"
+    },
+    secondary: {
+        light: "#FFD60A",
+        main: "#FFC300"
+    }
+}
+
+export default palette;
---
+++ REACT/frontend-laak/src/Theme/Styles/typography.js
@@ -0,0 +1,10 @@
+
+const typography = {
+    h1: {
+        fontSize: 48,
+        fontWeight: 500,
+        color: '#FFFFFF'
+    }
+}
+
+export default typography;
