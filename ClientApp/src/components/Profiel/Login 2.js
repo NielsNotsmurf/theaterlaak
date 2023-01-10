0b1887f8d1641aca04112dccf5baba8e32493a9e
nnspaans070 <36735011+nnspaans070@users.noreply.github.com>
2023-01-10 10:32:58 +0100

    aan homepage gewerkt

---
+++ ClientApp/src/components/Profiel/Login 2.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { useState } from "react";
+
+export default function Login(props) {
+    const [isLoading, setIsLoading] = useState(false);
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    return (
+        <div>
+            <h1>Login</h1>
+            <form>
+                <label>
+                    <p>Email</p>
+                    <input type="email" onChange={e => setEmail(e.target.value)} alt="email invoerveld"/>
+                </label>
+                <label>
+                    <p>Wachtwoord</p>
+                    <input type="password" onChange={e => setPassword(e.target.value)} alt="wachtwoord invoerveld"/>
+                </label>
+                <div>
+                    <button type="submit" alt="Login button">Submit</button>
+                </div>
+            </form>
+        </div>
+    );
+}
