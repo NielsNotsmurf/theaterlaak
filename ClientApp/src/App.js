2878f5c71af732729e911e1f5980b453d3192562
nnspaans070 <36735011+nnspaans070@users.noreply.github.com>
2023-01-11 13:15:10 +0100

    klene aanpassing

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,4 +1,4 @@
-import './custom.css';
+import '../src/./components/./styles/custom.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Kaarten } from './components/Profiel/Kaarten';
 import { Layout } from './components/Layout';
