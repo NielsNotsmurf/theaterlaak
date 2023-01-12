02e374f87ce4f451a9330c5d1b1a9df10beb4737
nnspaans070 <36735011+nnspaans070@users.noreply.github.com>
2023-01-12 16:14:27 +0100

    crud toegevoegd aan betrokkene

--- Controllers/BetrokkeneController.cs
+++ Controllers/BetrokkeneController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using theaterlaak.Converters;
 using theaterlaak.Data;
+using theaterlaak.Entities;
 using theaterlaak.Exceptions;
 
 namespace theaterlaak.Controllers;
@@ -41,5 +42,54 @@ public class BetrokkeneController : ControllerBase
             throw new NotFoundException();
 
         return betrokkene.ToDto(); 
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> addBetrokkene([FromBody] Models.Betrokkene betrokkene)
+    {
+        var newBetrokkene = new Betrokkene() {
+            Naam = betrokkene.Naam,
+            Beschrijving = betrokkene.Beschrijving,
+            Afbeelding = betrokkene.Afbeelding,
+            GeboorteDatum = betrokkene.GeboorteDatum,
+            TypePersoon = betrokkene.TypePersoon
+        };
+
+        _dbContext.Betrokkenen.Add(newBetrokkene);
+        await _dbContext.SaveChangesAsync();
+        return CreatedAtAction(nameof(GetBetrokkene), new { id = newBetrokkene.Id }, newBetrokkene.ToDto());
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult> updateBetrokkene(int id, [FromBody] Models.Betrokkene betrokkene)
+    {
+        var existingBetrokkene = await _dbContext.Betrokkenen.FirstOrDefaultAsync(v => v.Id == id);
+
+        if (existingBetrokkene == null)
+            throw new NotFoundException();
+
+        existingBetrokkene.Naam = betrokkene.Naam;
+        existingBetrokkene.Beschrijving = betrokkene.Beschrijving;
+        existingBetrokkene.Afbeelding = betrokkene.Afbeelding;
+        existingBetrokkene.GeboorteDatum = betrokkene.GeboorteDatum;
+        existingBetrokkene.TypePersoon = betrokkene.TypePersoon;
+
+
+        _dbContext.Betrokkenen.Update(existingBetrokkene);
+        await _dbContext.SaveChangesAsync();
+        return Ok(existingBetrokkene.ToDto());
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> deleteBetrokkene(int id)
+    {
+        var existingBetrokkene = await _dbContext.Betrokkenen.FirstOrDefaultAsync(v => v.Id == id);
+
+        if (existingBetrokkene == null)
+            throw new NotFoundException("Betrokkene is niet gevonden.");
+
+        _dbContext.Betrokkenen.Remove(existingBetrokkene);
+        await _dbContext.SaveChangesAsync();
+        return Ok();
     }
 }
