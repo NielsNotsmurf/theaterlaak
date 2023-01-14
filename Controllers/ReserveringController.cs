77e7aecc07fbb5b63e6ff8092a8394152c00ab95
AnwarTadmiri <20124783@student.hhs.nl>
2023-01-14 17:07:18 +0100

    reserveringservice gemaakt en reserveringcontroller aangepast

--- Controllers/ReserveringController.cs
+++ Controllers/ReserveringController.cs
@@ -4,6 +4,9 @@ using theaterlaak.Converters;
 using theaterlaak.Data;
 using theaterlaak.Exceptions;
 using theaterlaak.Entities;
+using aspnet_react_auth.Helpers;
+using theaterlaak.Services;
+using Microsoft.Extensions.Options;
 
 namespace theaterlaak.Controllers;
 
@@ -11,42 +14,27 @@ namespace theaterlaak.Controllers;
 [Route("/api/[controller]")]
 public class ReserveringController : ControllerBase
 {
-    private readonly ApplicationDbContext _dbContext;
-    public ReserveringController(ApplicationDbContext dbContext)
+    private IReserveringService _userService;
+    private readonly AppSettings _appSettings;
+
+    public ReserveringController(
+        IReserveringService userService,
+        IOptions<AppSettings> appSettings)
     {
-        _dbContext = dbContext;
+        _userService = userService;
+        _appSettings = appSettings.Value;
     }
 
     [HttpGet]
     public async Task<List<Models.Reservering>> GetReserveringen()
     {
-        var reserveringQuery = _dbContext.Reserveringen
-            .AsNoTracking()
-            .AsQueryable();
-
-        var reserveringen = await reserveringQuery.ToListAsync();
-        return reserveringen.ConvertAll(v => v.ToDto());
+        return await _userService.GetReserveringen();
     }
 
     [HttpGet("{id}")]
     public async Task<ActionResult<Models.Reservering>> GetReservering(int id)
     {
-        var reservering = await _dbContext.Reserveringen
-            .AsNoTracking()
-            .Include(r => r.Moment)
-                .ThenInclude(m => m!.Zaal)
-                    .ThenInclude(z => z!.Stoelen)
-             .Include(r => r.Moment)
-                .ThenInclude(m => m!.Voorstelling)
-            .Include(r => r.User)
-            .FirstOrDefaultAsync(v => v.Id == id);
-
-        if (reservering == null)
-            throw new NotFoundException("Reservering is niet gevonden.");
-
-        reservering.GereserveerdeStoelen = _dbContext.Stoelen.Where(s => s.ReserveringId == id).ToList();
-
-        return reservering.ToDto(); 
+        return await _userService.GetReservering(id);
     }
 
     [HttpPost]
@@ -55,27 +43,7 @@ public class ReserveringController : ControllerBase
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<ActionResult> AddReservering(Commands.AddOrUpdateReservering reservering)
     {
-        var newReservering = new Reservering
-        {
-            MomentId = reservering.MomentId,
-            UserId = reservering.UserId,
-        };
-
-        _dbContext.Add(newReservering);
-        await _dbContext.SaveChangesAsync();
-
-        reservering.GereserveerdeStoelenId.ForEach(async (stoelId) => {
-            var stoeltje = await _dbContext.Stoelen.FindAsync(stoelId);
-            if (stoeltje == null)
-                throw new NotFoundException("Stoeltje niet gevonden");
-
-            stoeltje.ReserveringId = newReservering.Id;
-            stoeltje.Bezet = true;
-        });
-
-        await _dbContext.SaveChangesAsync();
-
-        return Ok();
+        return await _userService.AddReservering(reservering);
     }
 
     [HttpPut("{id}")]
@@ -84,30 +52,7 @@ public class ReserveringController : ControllerBase
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<ActionResult> UpdateReservering(int id, Commands.AddOrUpdateReservering reservering)
     {
-        var updateReservering = await _dbContext.Reserveringen.Include(r => r.Moment).ThenInclude(m => m.Zaal).ThenInclude(z => z.Stoelen).FirstOrDefaultAsync(r => r.Id == id);
-        if (updateReservering == null)
-             throw new NotFoundException($"Reservering met ID '{id}' is niet gevonden.");
-
-        if (updateReservering.GereserveerdeStoelen != null && updateReservering.GereserveerdeStoelen.Count() > 0)
-        {
-            updateReservering.GereserveerdeStoelen.ForEach((stoel) => {
-                stoel.ReserveringId = null;
-                stoel.Bezet = false;
-            });
-        }
-
-        reservering.GereserveerdeStoelenId.ForEach(async (stoelId) => {
-            var stoeltje = await _dbContext.Stoelen.FindAsync(stoelId);
-            if (stoeltje == null)
-                throw new NotFoundException("Stoeltje niet gevonden");
-
-            stoeltje.ReserveringId = updateReservering.Id;
-            stoeltje.Bezet = true;
-        });
-
-        await _dbContext.SaveChangesAsync();
-
-        return NoContent();
+        return await _userService.UpdateReservering(id, reservering);
     }
 
     [HttpDelete("{id}")]
@@ -116,22 +61,7 @@ public class ReserveringController : ControllerBase
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<ActionResult> DeleteReservering(int id)
     {
-        var deleteReservering = await _dbContext.Reserveringen.Include(r => r.Moment).ThenInclude(m => m.Zaal).ThenInclude(z => z.Stoelen).FirstOrDefaultAsync(r => r.Id == id);
-        if (deleteReservering == null)
-            throw new NotFoundException($"Voorstelling met ID '{id}' is niet gevonden.");
-
-        if (deleteReservering.GereserveerdeStoelen != null && deleteReservering.GereserveerdeStoelen.Count() > 0)
-        {
-            deleteReservering.GereserveerdeStoelen.ForEach((stoel) => {
-                stoel.ReserveringId = null;
-                stoel.Bezet = false;
-            });
-        }
-
-        _dbContext.Reserveringen.Remove(deleteReservering);
-        await _dbContext.SaveChangesAsync();
-
-        return NoContent();
+        return await _userService.DeleteReservering(id);
     }
 
     [HttpGet("[action]/{userId}")]
@@ -139,14 +69,6 @@ public class ReserveringController : ControllerBase
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public async Task<List<Models.Reservering>> GetReserveringenByUserId(string userId)
     {
-        var reserveringQuery = _dbContext.Reserveringen
-            .AsNoTracking()
-            .Include(r => r.Moment)
-                .ThenInclude(m => m!.Voorstelling)
-            .Where(v => v.UserId == userId)
-            .AsQueryable();
-
-        var reserveringen = await reserveringQuery.ToListAsync();
-        return reserveringen.ConvertAll(r => r.ToDto());
+        return await _userService.GetReserveringenByUserId(userId);
     }
 }
---
+++ Services/reserveringService.cs
@@ -0,0 +1,154 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using theaterlaak.Commands;
+using theaterlaak.Converters;
+using theaterlaak.Data;
+using theaterlaak.Exceptions;
+using theaterlaak.Models;
+
+namespace theaterlaak.Services
+{
+    public interface IReserveringService
+    {
+        Task<List<Models.Reservering>> GetReserveringen();
+        Task<ActionResult<Models.Reservering>> GetReservering(int id);
+        Task<ActionResult> AddReservering(Commands.AddOrUpdateReservering reservering);
+        Task<ActionResult> UpdateReservering(int id, Commands.AddOrUpdateReservering reservering);
+        Task<ActionResult> DeleteReservering(int id);
+        Task<List<Models.Reservering>> GetReserveringenByUserId(string userId);
+    }
+
+    public class reserveringService : IReserveringService
+    {
+        private ApplicationDbContext _context;
+
+        public reserveringService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Reservering>> GetReserveringen()
+        {
+            var reserveringQuery = _context.Reserveringen
+            .AsNoTracking()
+            .AsQueryable();
+
+            var reserveringen = await reserveringQuery.ToListAsync();
+            return reserveringen.ConvertAll(v => v.ToDto());
+        }
+
+        public async Task<ActionResult<Reservering>> GetReservering(int id)
+        {
+            var reservering = await _context.Reserveringen
+            .AsNoTracking()
+            .Include(r => r.Moment)
+                .ThenInclude(m => m!.Zaal)
+                    .ThenInclude(z => z!.Stoelen)
+             .Include(r => r.Moment)
+                .ThenInclude(m => m!.Voorstelling)
+            .Include(r => r.User)
+            .FirstOrDefaultAsync(v => v.Id == id);
+
+            if (reservering == null)
+                throw new NotFoundException("Reservering is niet gevonden.");
+
+            reservering.GereserveerdeStoelen = _context.Stoelen.Where(s => s.ReserveringId == id).ToList();
+
+            return reservering.ToDto();
+        }
+
+        public async Task<ActionResult> AddReservering(AddOrUpdateReservering reservering)
+        {
+            var newReservering = new Reservering
+            {
+                MomentId = reservering.MomentId,
+                UserId = reservering.UserId,
+            };
+
+            _context.Add(newReservering);
+            await _context.SaveChangesAsync();
+
+            reservering.GereserveerdeStoelenId.ForEach(async (stoelId) =>
+            {
+                var stoeltje = await _context.Stoelen.FindAsync(stoelId);
+                if (stoeltje == null)
+                    throw new NotFoundException("Stoeltje niet gevonden");
+
+                stoeltje.ReserveringId = newReservering.Id;
+                stoeltje.Bezet = true;
+            });
+
+            await _context.SaveChangesAsync();
+
+            // return newReservering.ToDto();
+            return new OkResult();
+        }
+        public async Task<ActionResult> UpdateReservering(int id, AddOrUpdateReservering reservering)
+        {
+            var updateReservering = await _context.Reserveringen.Include(r => r.Moment).ThenInclude(m => m.Zaal).ThenInclude(z => z.Stoelen).FirstOrDefaultAsync(r => r.Id == id);
+            if (updateReservering == null)
+                throw new NotFoundException($"Reservering met ID '{id}' is niet gevonden.");
+
+            if (updateReservering.GereserveerdeStoelen != null && updateReservering.GereserveerdeStoelen.Count() > 0)
+            {
+                updateReservering.GereserveerdeStoelen.ForEach((stoel) =>
+                {
+                    stoel.ReserveringId = null;
+                    stoel.Bezet = false;
+                });
+            }
+
+            reservering.GereserveerdeStoelenId.ForEach(async (stoelId) =>
+            {
+                var stoeltje = await _context.Stoelen.FindAsync(stoelId);
+                if (stoeltje == null)
+                    throw new NotFoundException("Stoeltje niet gevonden");
+
+                stoeltje.ReserveringId = updateReservering.Id;
+                stoeltje.Bezet = true;
+            });
+
+            await _context.SaveChangesAsync();
+
+            //return NoContent();
+            return new OkResult();
+
+        }
+        public async Task<ActionResult> DeleteReservering(int id)
+        {
+            var deleteReservering = await _context.Reserveringen.Include(r => r.Moment).ThenInclude(m => m.Zaal).ThenInclude(z => z.Stoelen).FirstOrDefaultAsync(r => r.Id == id);
+            if (deleteReservering == null)
+                throw new NotFoundException($"Voorstelling met ID '{id}' is niet gevonden.");
+
+            if (deleteReservering.GereserveerdeStoelen != null && deleteReservering.GereserveerdeStoelen.Count() > 0)
+            {
+                deleteReservering.GereserveerdeStoelen.ForEach((stoel) =>
+                {
+                    stoel.ReserveringId = null;
+                    stoel.Bezet = false;
+                });
+            }
+
+            _context.Reserveringen.Remove(deleteReservering);
+            await _context.SaveChangesAsync();
+
+            // return NoContent();
+            return new OkResult();
+        }
+
+        public async Task<List<Models.Reservering>> GetReserveringenByUserId(string userId)
+        {
+            var reserveringQuery = _context.Reserveringen
+            .AsNoTracking()
+            .Include(r => r.Moment)
+                .ThenInclude(m => m!.Voorstelling)
+            .Where(v => v.UserId == userId)
+            .AsQueryable();
+
+            var reserveringen = await reserveringQuery.ToListAsync();
+            return reserveringen.ConvertAll(r => r.ToDto());
+        }
+
+
+    }
+}
