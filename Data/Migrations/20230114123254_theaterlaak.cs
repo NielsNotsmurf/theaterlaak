5981a1283d56f955c9daf3a7e560aa184178f305
AnwarTadmiri <20124783@student.hhs.nl>
2023-01-14 13:41:19 +0100

    salt table in de db toegevoegd

---
+++ Data/Migrations/20230114123254_theaterlaak.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace theaterlaak.Migrations
+{
+    public partial class theaterlaak : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PasswordSalt",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasswordSalt",
+                table: "AspNetUsers");
+        }
+    }
+}
