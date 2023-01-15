5736ff706ff17b470059aa2bd283596f8e7a2e84
AnwarTadmiri <20124783@student.hhs.nl>
2023-01-15 15:58:49 +0100

    BetrokkeneService gemaakt en betrokkenecontroller aangepast

---
+++ Services/betrokkeneService.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using theaterlaak.Converters;
+using theaterlaak.Data;
+using theaterlaak.Exceptions;
+using theaterlaak.Models;
+
+namespace theaterlaak.Services
+{
+    public interface IBetrokkeneService
+    {
+        Task<List<Models.Betrokkene>> GetBetrokkenen();
+        Task<ActionResult<Models.Betrokkene>> GetBetrokkene(int id);
+    }
+
+    public class betrokkeneService : IBetrokkeneService
+    {
+        private ApplicationDbContext _context;
+
+        public betrokkeneService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<List<Models.Betrokkene>> GetBetrokkenen()
+        {
+            var betrokkenenQuery = _context.Betrokkenen
+            .AsNoTracking()
+            .AsQueryable();
+
+            var betrokkenen = await betrokkenenQuery.ToListAsync();
+            return betrokkenen.ConvertAll(v => v.ToDto());
+        }
+
+        public async Task<ActionResult<Models.Betrokkene>> GetBetrokkene(int id)
+        {
+            var betrokkene = await _context.Betrokkenen
+            .AsNoTracking()
+            .FirstOrDefaultAsync(v => v.Id == id);
+
+            if (betrokkene == null)
+                throw new NotFoundException();
+
+            return betrokkene.ToDto();
+        }
+    }
+}
